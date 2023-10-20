f942f7fcc85bda846017755b7024c0ca547a2f6d
GaoTao <gaotaonull@gmail.com>
2023-10-20 16:49:02 +0800

    first commit

---
+++ src/views/dashboard/admin/index.vue
@@ -0,0 +1,185 @@
+<template>
+  <div class="dashboard-editor-container">
+   <h4>欢迎登录上海信息技术学校专业建设与发展质量评估系统</h4>
+   <h4>请各专业老师按照各指标的填报要求，如实填报并上传相关证明材料</h4>
+    <!-- <github-corner class="github-corner" /> -->
+
+    <panel-group @handleSetLineChartData="handleSetLineChartData" />
+
+    <!-- <el-row style="background:#fff;padding:16px 16px 0;margin-bottom:32px;">
+      <line-chart :chart-data="lineChartData" />
+    </el-row> -->
+
+    <el-row :gutter="32">
+      <!-- <el-col :xs="24" :sm="24" :lg="8">
+        <div class="chart-wrapper">
+          <raddar-chart />
+        </div>
+      </el-col> -->
+      
+      <el-col :xs="24" :sm="24" :lg="8">
+            <div>审批情况总览 </div>
+            系部：
+            <el-select v-model="value" class="m-2" @change="$forceUpdate()"  placeholder="" size="small">
+              <el-option
+                v-for="item in options"
+                :key="item.value"
+                :label="item.label"
+                :value="item.value"
+              />
+            </el-select>
+              专业：
+                <el-select v-model="value" class="m-2" @change="$forceUpdate()" placeholder="" size="small">
+                  <el-option
+                    v-for="item in options"
+                    :key="item.value"
+                    :label="item.label"
+                    :value="item.value"
+                  />
+                </el-select>
+
+        <div class="chart-wrapper">
+          <pie-chart />
+        </div>
+      </el-col>
+
+      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px">
+        <el-row :gutter="32" >
+        <todo-list />
+        </el-row>
+        
+        <el-row :gutter="32">
+        <todo-list1 />
+        </el-row>
+      </el-col>
+      
+    </el-row>
+
+    <!-- <el-row :gutter="8">
+      <el-col :xs="{span: 24}" :sm="{span: 24}" :md="{span: 24}" :lg="{span: 12}" :xl="{span: 12}" style="padding-right:8px;margin-bottom:30px;">
+        <transaction-table />
+      </el-col>
+      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+        <todo-list />
+      </el-col>
+      <el-col :xs="{span: 24}" :sm="{span: 12}" :md="{span: 12}" :lg="{span: 6}" :xl="{span: 6}" style="margin-bottom:30px;">
+        <box-card />
+      </el-col>
+    </el-row> -->
+  </div>
+</template>
+
+<script>
+import GithubCorner from '@/components/GithubCorner'
+import PanelGroup from './components/PanelGroup'
+import LineChart from './components/LineChart'
+import RaddarChart from './components/RaddarChart'
+import PieChart from './components/PieChart'
+import BarChart from './components/BarChart'
+import TransactionTable from './components/TransactionTable'
+import TodoList from './components/TodoList'
+import TodoList1 from './components/TodoList1'
+import BoxCard from './components/BoxCard'
+
+const lineChartData = {
+  newVisitis: {
+    expectedData: [100, 120, 161, 134, 105, 160, 165],
+    actualData: [120, 82, 91, 154, 162, 140, 145]
+  },
+  messages: {
+    expectedData: [200, 192, 120, 144, 160, 130, 140],
+    actualData: [180, 160, 151, 106, 145, 150, 130]
+  },
+  purchases: {
+    expectedData: [80, 100, 121, 104, 105, 90, 100],
+    actualData: [120, 90, 100, 138, 142, 130, 130]
+  },
+  shoppings: {
+    expectedData: [130, 140, 141, 142, 145, 150, 160],
+    actualData: [120, 82, 91, 154, 162, 140, 130]
+  }
+}
+
+const options = [
+  {
+    value: '智能制造',
+    label: '智能制造',
+  },
+  {
+    value: '材料与检测',
+    label: '材料与检测',
+  },
+  {
+    value: '信息技术系',
+    label: '信息技术系',
+  },
+  {
+    value: '商务管理系',
+    label: '商务管理系',
+  },
+  {
+    value: '中本贯通',
+    label: '中本贯通',
+  },
+]
+
+
+export default {
+  name: 'DashboardAdmin',
+  components: {
+    GithubCorner,
+    PanelGroup,
+    LineChart,
+    RaddarChart,
+    PieChart,
+    BarChart,
+    TransactionTable,
+    TodoList,
+    TodoList1,
+    BoxCard
+  },
+  data() {
+    return {
+      options
+    }
+    // return {
+    //   lineChartData: lineChartData.newVisitis
+    // }
+  },
+  methods: {
+    handleSetLineChartData(type) {
+      this.lineChartData = lineChartData[type]
+    }
+  }
+//   abc(val) {
+//   this.$set(this.value, val )
+// }
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard-editor-container {
+  padding: 32px;
+  background-color: rgb(240, 242, 245);
+  position: relative;
+
+  .github-corner {
+    position: absolute;
+    top: 0px;
+    border: 0;
+    right: 0;
+  }
+
+  .chart-wrapper {
+    background: #fff;
+    padding: 126px 66px 0;
+    margin-bottom: 62px;
+  }
+}
+
+@media (max-width:1024px) {
+  .chart-wrapper {
+    padding: 8px;
+  }
+}
+</style>
---
+++ src/views/nested/menu1/index.vue
@@ -0,0 +1,62 @@
+<template>
+  <div class="grid-content bg-purple">
+    <el-tree
+      :data="data"
+      node-key="id"
+      :default-expanded-keys="[2, 3]"
+      :default-checked-keys="[5]"
+    >
+    </el-tree>
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      data: [
+        {
+          id: 1,
+          label: "一级 2",
+          children: [
+            {
+              id: 3,
+              label: "二级 2-1",
+              children: [
+                {
+                  id: 4,
+                  label: <a href="https://www.w3schools.com">Visit W3Schools</a>,
+                },
+                {
+                  id: 5,
+                  label: "三级 3-1-2",
+                  disabled: true,
+                },
+              ],
+            },
+            {
+              id: 2,
+              label: "二级 2-2",
+              disabled: true,
+              children: [
+                {
+                  id: 6,
+                  label: "三级 3-2-1",
+                },
+                {
+                  id: 7,
+                  label: "三级 3-2-2",
+                  disabled: true,
+                },
+              ],
+            },
+          ],
+        },
+      ],
+      defaultProps: {
+        children: "children",
+        label: "label",
+      },
+    };
+  },
+};
+</script>
