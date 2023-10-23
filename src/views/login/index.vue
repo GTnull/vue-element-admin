be2f0b1517a2ee60838ee47f771a0dec287abc53
gaozheng <17806256336@163.com>
2023-10-23 13:18:08 +0800

    update(手册ui)

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,36 +1,72 @@
 <template>
   <div class="div_1">
-    <!-- <img src="@/assets/背景.png"> -->
-    <!-- <div class="div_1"><img src="@/assets/背景.png"></div> -->
     <div class="div_2">
-      <!-- <div class="div_logo"><img src="@/assets/log.png"></div>
-      <div class="div_name">上海信息技术学校</div>
-      <div class="div_name2">专业建设与发展评估</div>
-      <div class="div_name3">Welcome to Shanghai Information Technology College</div> -->
       <div class="div_ui"><img src="@/assets/ui.png"></div>
-      <el-input class="blurred-input" />
+      <div class="div-login">
+        <div class="div-login-input">
+          <el-input
+            v-model="loginForm.username"
+            size="medium"
+            class="username-password-input"
+          />
+          <el-input
+            v-model="loginForm.password"
+            class="username-password-input"
+            show-password
+            style="margin-top: 19px"
+          />
+        </div>
+        <div class="div-login-button">
+          <el-button type="primary" class="button-login">登录</el-button>
+        </div>
+      </div>
       <!-- <div><el-input>abc</el-input></div> -->
     </div>
 
     <!-- <div class="div_3"> abc</div> -->
   </div>
-
 </template>
 
 <script>
-
 export default {
   name: '',
 
   data() {
+    const validateUsername = (rule, value, callback) => {
+      // 检查用户是否正确
+      const valid_map = [
+        'admin',
+        'editor',
+        'user',
+        'leader',
+        'reviewer',
+        'reporter'
+      ]
+      if (valid_map.indexOf(value.trim()) < 0) {
+        callback(new Error('Please enter the correct user name'))
+      } else {
+        callback()
+      }
+    }
+    const validatePassword = (rule, value, callback) => {
+      if (value.length < 6) {
+        callback(new Error('The password can not be less than 6 digits'))
+      } else {
+        callback()
+      }
+    }
     return {
       loginForm: {
         username: 'admin',
         password: '111111'
       },
       loginRules: {
-        username: [{ required: true, trigger: 'blur', validator: validateUsername }],
-        password: [{ required: true, trigger: 'blur', validator: validatePassword }]
+        username: [
+          { required: true, trigger: 'blur', validator: validateUsername }
+        ],
+        password: [
+          { required: true, trigger: 'blur', validator: validatePassword }
+        ]
       },
       passwordType: 'password',
       capsTooltip: false,
@@ -65,9 +101,7 @@ export default {
   destroyed() {
     // window.removeEventListener('storage', this.afterQRScan)
   },
-  methods: {
-
-  }
+  methods: {}
 }
 </script>
 
@@ -89,104 +123,29 @@ export default {
   width: 1652px;
   height: 913px;
 }
-
-// .div_1 {
-//   background-image: url('../../assets/背景.png');
-//   background-size: cover; /* 图像大小适应容器 */
-//   background-position: center; /* 图像在容器中的位置 */
-//   background-repeat: no-repeat; /* 禁止图像平铺 */
-//   width: 100%;
-//   height: 100%;
-//   // width: 1920px;
-//   // height: 1080px;
-//   // background: linear-gradient(360deg, #0B4F96 0%, rgba(27,83,140,0.6) 72%, rgba(11,79,150,0) 100%);
-//   // border-radius: 0px 0px 0px 0px;
-//   // opacity: 1;
-// }
-// .div_2 {
-//   z-index: 1;
-//   margin-left: 86px;
-//   margin-bottom: 61px;
-//   margin-top: 106px;
-//   margin-right: 182px;
-//   width: 1652px;
-//   height: 913px;
-//   background-color: red;
-// }
-.div_logo {
-  width:68.17px;
-  height: 260px;
-}
-.div_name {
-
-  width: 772px;
-height: 126px;
-font-size: 80px;
-font-family: Alibaba PuHuiTi 2.0-65 Medium, Alibaba PuHuiTi 20;
-font-weight: normal;
-color: #FFFFFF;
-line-height: 105px;
-letter-spacing: 9px;
-text-shadow: 0px 4px 4px rgba(0,0,0,0.25);
--webkit-background-clip: text;
-// -webkit-text-fill-color: transparent;
-}
-.div_name2 {
-  width: 870px;
-height: 126px;
-font-size: 80px;
-font-family: Alibaba PuHuiTi 2.0-65 Medium, Alibaba PuHuiTi 20;
-font-weight: normal;
-color: #FFFFFF;
-line-height: 105px;
-letter-spacing: 9px;
-text-shadow: 0px 4px 4px rgba(0,0,0,0.25);
--webkit-background-clip: text;
-// -webkit-text-fill-color: transparent;
-}
-.div_name3 {
-  width: 959px;
-height: 180px;
-font-size: 48px;
-font-family: Alibaba PuHuiTi 2.0-55 Regular, Alibaba PuHuiTi 20;
-font-weight: normal;
-color: #FFFFFF;
-line-height: 90px;
-text-shadow: 0px 4px 4px rgba(0,0,0,0.25);
--webkit-background-clip: text;
-// -webkit-text-fill-color: transparent;
-}
 .div_ui {
+  position: absolute;
   width: 460px;
-height: 490px;
-box-shadow: 0px 4px 16px 0px rgba(0,0,0,0.15);
-border-radius: 18px 18px 18px 18px;
-opacity: 1;
+  height: 490px;
+  box-shadow: 0px 4px 16px 0px rgba(0, 0, 0, 0.15);
+  border-radius: 18px 18px 18px 18px;
+  opacity: 1;
 }
-.div_5 {
-  width: 772px;
-height: 126px;
-font-size: 90px;
-font-family: Alibaba PuHuiTi 2.0-65 Medium, Alibaba PuHuiTi 20;
-font-weight: normal;
-color: #FFFFFF;
-line-height: 105px;
-letter-spacing: 9px;
-text-shadow: 0px 4px 4px rgba(0,0,0,0.25);
--webkit-background-clip: text;
--webkit-text-fill-color: transparent;
-}
-.div_6 {
+.div-login {
+  position: absolute;
+  margin-top: 189px;
+  right: 182px;
   width: 460px;
-height: 490px;
-background: #FFFFFF;
-box-shadow: 0px 4px 16px 0px rgba(0,0,0,0.15);
-border-radius: 18px 18px 18px 18px;
-opacity: 1;
+  height: 490px;
 }
 
-.blurred-input {
-  width: 300px;
+.div-login-input {
+  position: absolute;
+  margin-top: 160px;
+  margin-left: 75px;
+}
+.username-password-input {
+  width: 350px;
   padding: 10px;
   background-color: rgba(0, 0, 0, 0); /* 完全透明 */
   // background: rgba(255, 255, 255, 0.8); /* 设置输入框背景颜色，增强虚化效果 */
@@ -196,11 +155,25 @@ opacity: 1;
   // box-shadow: 0 0 10px rgba(0, 0, 0, 0.3); /* 添加阴影效果，以增强虚化效果 */
   // transition: background 0.3s, filter 0.3s; /* 添加过渡效果，以平滑变化 */
 }
-
-.blurred-input:focus {
-  background: rgba(255, 255, 255, 1); /* 输入框获取焦点时，取消虚化 */
-  filter: blur(0);
+.div-login-button {
+  position: absolute;
+  margin-top: 313px;
+  margin-left: 40px;
+  margin-right: 40px;
+  width: 380px;
+  height: 60px;
 }
-
+.button-login {
+  width: 100%;
+  height: 100%;
+  // 圆角
+  border-radius: 8px 8px 8px 8px;
+  // 从左到右渐变
+  background: linear-gradient(to right, #0fadf1, #3466e4);
+}
+// 覆盖默认的边框
+.el-input__inner {
+  border: none;
+  border-bottom: 1px solid #ccdee7;
+}
 </style>
-
